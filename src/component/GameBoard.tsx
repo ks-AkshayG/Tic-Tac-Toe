6ba804dad8698c9173589a3b3ae79768cce37c25
Akshay G <akshay.guna@kalkani.in>
2024-02-24 11:29:11 +0530

    Individual browser controll

--- src/component/GameBoard.tsx
+++ src/component/GameBoard.tsx
@@ -17,6 +17,7 @@ import {
   winConditions,
 } from "../constants/ConstantValue";
 import { GetSingleUserData } from "../pages/Game";
+import { useLocalStorage } from "usehooks-ts";
 
 type GameBoardProps = {
   data: GetSingleUserData;
@@ -25,7 +26,7 @@ type GameBoardProps = {
 const GameBoard = ({ data }: GameBoardProps) => {
   const { GameID } = useParams();
   const queryClient = useQueryClient();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // console.log(data)
 
@@ -43,6 +44,16 @@ const GameBoard = ({ data }: GameBoardProps) => {
   const [currentTurn, setCurrentTurn] = useState(
     data.turnO ? "Current Turn - O" : "Current Turn - X"
   );
+  const [character, setCharacter] = useLocalStorage<"O" | "X" | undefined>("charater", undefined);
+  const [value, setValue] = useState("");
+
+  const handleCharater = () => {
+
+    if (value === "O" || value === "X") {
+      setCharacter(value);
+    }
+  };
+  // console.log(character);
 
   const handleUpdateData = async () => {
     const supares = await Supabase.from("tic-tac-toe")
@@ -132,8 +143,7 @@ const GameBoard = ({ data }: GameBoardProps) => {
   }, [data.turnO]);
 
   const handleClick = (i: number) => {
-    if (state[i] !== "") return;
-    if (winner !== "") return;
+    if (state[i] !== "" || winner !== "" || character !== "O") return;
 
     if (turnO) {
       let newValueArray = [...state];
@@ -151,8 +161,7 @@ const GameBoard = ({ data }: GameBoardProps) => {
   };
 
   const handleDoubleClick = (i: number) => {
-    if (state[i] !== "") return;
-    if (winner !== "") return;
+    if (state[i] !== "" || winner !== "" || character !== "X") return;
 
     if (!turnO) {
       let newValueArray = [...state];
@@ -232,7 +241,7 @@ const GameBoard = ({ data }: GameBoardProps) => {
 
   const handleResetGame = async () => {
     await DeleteGameRefetch();
-    navigate('/')
+    navigate("/");
   };
 
   const hidden: React.CSSProperties =
@@ -273,106 +282,124 @@ const GameBoard = ({ data }: GameBoardProps) => {
 
   return (
     <div className=" w-full flex flex-col justify-center items-center">
-      {ScoreData && (
-        <div className=" w-full flex flex-row justify-around">
-          <Score character="O" score={ScoreData.countScoreO} />
-          <Score character="Draw" score={ScoreData.countScoreDraw} />
-          <Score character="X" score={ScoreData.countScoreX} />
+      {!character && (
+        <div>
+          <h2 className="my-3 text-[30px]">Enter your character</h2>
+          <div className="w-full flex justify-center flex-row">
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              placeholder="'O' or 'X'"
+              className="text-green-600 px-3"
+            />
+            <button
+              onClick={handleCharater}
+              className="border border-black mx-2 px-3 rounded-md"
+            >
+              Select
+            </button>
+          </div>
+          {value !== "" && value !== "X" && value !== "O" && (
+            <p className="my-2 text-red-600">Enter the correct choice</p>
+          )}
         </div>
       )}
-      <div className="w-full flex flex-row justify-evenly items-center">
-        {ScoreData && (
-          <div>
-            <Chances chances={ScoreData.countO} />
+      {character && ScoreData && (
+        <div>
+          <div className=" w-full flex flex-row justify-around">
+            <Score character="O" score={ScoreData.countScoreO} />
+            <Score character="Draw" score={ScoreData.countScoreDraw} />
+            <Score character="X" score={ScoreData.countScoreX} />
           </div>
-        )}
-        <div className="flex flex-col">
-          <div className="text-center text-[40px] mb-5">
-            <CurrentTurn turn={winnerAnnounce()} />
+          <div className="w-full flex flex-row justify-evenly items-center">
+            <div>
+              <Chances chances={ScoreData.countO} />
+            </div>
+            <div className="flex flex-col">
+              <div className="text-center text-[40px] mb-5">
+                <CurrentTurn turn={winnerAnnounce()} />
+              </div>
+              <div className="flex justify-center items-center flex-row text-[100px]">
+                <SingleSquareBoard
+                  value={ScoreData.state[0]}
+                  onClick={() => handleClick(0)}
+                  onDoubleClick={() => handleDoubleClick(0)}
+                />
+                <SingleSquareBoard
+                  value={ScoreData.state[1]}
+                  onClick={() => handleClick(1)}
+                  onDoubleClick={() => handleDoubleClick(1)}
+                />
+                <SingleSquareBoard
+                  value={ScoreData.state[2]}
+                  onClick={() => handleClick(2)}
+                  onDoubleClick={() => handleDoubleClick(2)}
+                />
+              </div>
+              <div className="flex justify-center items-center flex-row text-[100px]">
+                <SingleSquareBoard
+                  value={ScoreData.state[3]}
+                  onClick={() => handleClick(3)}
+                  onDoubleClick={() => handleDoubleClick(3)}
+                />
+                <SingleSquareBoard
+                  value={ScoreData.state[4]}
+                  onClick={() => handleClick(4)}
+                  onDoubleClick={() => handleDoubleClick(4)}
+                />
+                <SingleSquareBoard
+                  value={ScoreData.state[5]}
+                  onClick={() => handleClick(5)}
+                  onDoubleClick={() => handleDoubleClick(5)}
+                />
+              </div>
+              <div className="flex justify-center items-center flex-row text-[100px]">
+                <SingleSquareBoard
+                  value={ScoreData.state[6]}
+                  onClick={() => handleClick(6)}
+                  onDoubleClick={() => handleDoubleClick(6)}
+                />
+                <SingleSquareBoard
+                  value={ScoreData.state[7]}
+                  onClick={() => handleClick(7)}
+                  onDoubleClick={() => handleDoubleClick(7)}
+                />
+                <SingleSquareBoard
+                  value={ScoreData.state[8]}
+                  onClick={() => handleClick(8)}
+                  onDoubleClick={() => handleDoubleClick(8)}
+                />
+              </div>
+              <div>
+                <button
+                  style={hidden}
+                  className="border border-green-600 mt-[30px] py-[15px] px-[30px] rounded-3xl hover:bg-green-600 "
+                  onClick={handleReset}
+                >
+                  New Game
+                </button>
+              </div>
+            </div>
+            <div>
+              <Chances chances={ScoreData.countX} />
+            </div>
           </div>
-          {ScoreData && (
-            <div className="flex justify-center items-center flex-row text-[100px]">
-              <SingleSquareBoard
-                value={ScoreData.state[0]}
-                onClick={() => handleClick(0)}
-                onDoubleClick={() => handleDoubleClick(0)}
-              />
-              <SingleSquareBoard
-                value={ScoreData.state[1]}
-                onClick={() => handleClick(1)}
-                onDoubleClick={() => handleDoubleClick(1)}
-              />
-              <SingleSquareBoard
-                value={ScoreData.state[2]}
-                onClick={() => handleClick(2)}
-                onDoubleClick={() => handleDoubleClick(2)}
-              />
+          <div className=" w-[80vw] text-end flex flex-col justify-end items-end relative">
+            <div className=" absolute bottom-14">
+              {menu && (
+                <MenuButton
+                  resetBoard={handleReset}
+                  resetGame={handleResetGame}
+                />
+              )}
             </div>
-          )}
-          {ScoreData && (
-            <div className="flex justify-center items-center flex-row text-[100px]">
-              <SingleSquareBoard
-                value={ScoreData.state[3]}
-                onClick={() => handleClick(3)}
-                onDoubleClick={() => handleDoubleClick(3)}
-              />
-              <SingleSquareBoard
-                value={ScoreData.state[4]}
-                onClick={() => handleClick(4)}
-                onDoubleClick={() => handleDoubleClick(4)}
-              />
-              <SingleSquareBoard
-                value={ScoreData.state[5]}
-                onClick={() => handleClick(5)}
-                onDoubleClick={() => handleDoubleClick(5)}
-              />
-            </div>
-          )}
-          {ScoreData && (
-            <div className="flex justify-center items-center flex-row text-[100px]">
-              <SingleSquareBoard
-                value={ScoreData.state[6]}
-                onClick={() => handleClick(6)}
-                onDoubleClick={() => handleDoubleClick(6)}
-              />
-              <SingleSquareBoard
-                value={ScoreData.state[7]}
-                onClick={() => handleClick(7)}
-                onDoubleClick={() => handleDoubleClick(7)}
-              />
-              <SingleSquareBoard
-                value={ScoreData.state[8]}
-                onClick={() => handleClick(8)}
-                onDoubleClick={() => handleDoubleClick(8)}
-              />
-            </div>
-          )}
-          <div>
-            <button
-              style={hidden}
-              className="border border-green-600 mt-[30px] py-[15px] px-[30px] rounded-3xl hover:bg-green-600 "
-              onClick={handleReset}
-            >
-              New Game
+            <button onClick={handleMenu}>
+              <img src={MenuIcon} alt="Menu" className="w-[57px]" />
             </button>
           </div>
         </div>
-        {ScoreData && (
-          <div>
-            <Chances chances={ScoreData.countX} />
-          </div>
-        )}
-      </div>
-      <div className=" w-[80vw] text-end flex flex-col justify-end items-end relative">
-        <div className=" absolute bottom-14">
-          {menu && (
-            <MenuButton resetBoard={handleReset} resetGame={handleResetGame} />
-          )}
-        </div>
-        <button onClick={handleMenu}>
-          <img src={MenuIcon} alt="Menu" className="w-[57px]" />
-        </button>
-      </div>
+      )}
     </div>
   );
 };
